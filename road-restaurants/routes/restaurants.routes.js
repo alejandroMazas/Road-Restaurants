65f0df3d70541afd471ca0bcf88f6d669f857d38
Alejandro Mazas de Lizana <alejandro.mazasdelizana@gmail.com>
2022-04-26 19:06:51 +0200

    crud restaurants done

--- road-restaurants/routes/restaurants.routes.js
+++ road-restaurants/routes/restaurants.routes.js
@@ -9,13 +9,14 @@ router.get('/restaurants/create', (req, res) => {
 
 router.post('/restaurants/create', (req, res) => {
 
-    const { restaurantname, image, type, description, rating, quality, service, ambience, opinion } = req.body
+    const { restaurantname, image, type, place, description, rating, quality, service, ambience, opinion } = req.body
 
     Restaurant
         .create({
             name: restaurantname,
             image,
             type,
+            place,
             description,
             rating,
             ratingDetails: { qualityPrice: quality, service: service, ambience: ambience, },
@@ -49,7 +50,52 @@ router.get('/restaurants/details/:id', (req, res) => {
         .catch(err => console.log(err))
 })
 
+router.get('/restaurants/details/:id/edit', (req, res) => {
 
+    const { id } = req.params
+
+    Restaurant
+        .findById(id)
+        .then(restaurant => {
+            res.render('restaurants/update-form', restaurant)
+        })
+        .catch(err => console.log(err))
+})
+
+router.post('/restaurants/details/:id/edit', (req, res) => {
+
+    const { id } = req.params
+
+    const { restaurantname, image, type, place, description, rating, quality, service, ambience, opinion } = req.body
+
+    Restaurant
+        .findByIdAndUpdate(id, {
+            name: restaurantname,
+            image,
+            type,
+            place,
+            description,
+            rating,
+            ratingDetails: { qualityPrice: quality, service: service, ambience: ambience, },
+            opinion
+        }, { new: true })
+        .then(restaurant => {
+            res.redirect(`/restaurants/details/${restaurant.id}`)
+        })
+        .catch(err => console.log(err))
+})
+
+router.post('/restaurants/:id/delete', (req, res) => {
+
+    const { id } = req.params
+
+    Restaurant
+        .findByIdAndDelete(id)
+        .then(() => {
+            res.redirect('/restaurants')
+        })
+        .catch(err => console.log(err))
+})
 
 
 module.exports = router
